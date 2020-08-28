f30246f62b53d0a51c581475be749fc2c17a3a5e
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-28 16:56:51 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,7 @@ def bonus
    }
   }
 
-  epic_tragedy["capulet"]["status"]<<"dead"
-  epic_tragedy ["montague"]["hero"]["status"]<<"dead"
-  
+  epic_tragedy[:capulet][:status]<<"dead"
+
   epic_tragedy
 end
